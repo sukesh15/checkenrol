1a472d9c0a5067263506f85fa2030db6a1b65f3c
Perryn Fowler <perryn.fowler@gmail.com>
2013-01-09 11:41:58 +1100

    enrolment check class working

--- app/controllers/check_enrolment_controller.rb
+++ app/controllers/check_enrolment_controller.rb
@@ -1,6 +1,4 @@
 class CheckEnrolmentController < ApplicationController
-  include HTTParty
-  base_uri 'https://oevf.aec.gov.au'
     
   def show
     @captcha_image = CaptchaImage.new
@@ -9,34 +7,9 @@ class CheckEnrolmentController < ApplicationController
   end
   
   def check
-    options = {
-      :body => {
-        "__LASTFOCUS" => "",
-        "ctl00_ContentPlaceHolderBody_ToolkitScriptManager1_HiddenField" => "",
-        "__EVENTTARGET"=>"",
-        "__EVENTARGUMENT"=>"",
-        "__VIEWSTATE"=>"/wEPDwUJNDY5MTk1MTI1D2QWAmYPZBYEAgEPZBYCAhsPFgIeB2NvbnRlbnQFHENoZWNrIG15IGVsZWN0b3JhbCBlbnJvbG1lbnRkAgMPZBYGAgwPFgIeCWlubmVyaHRtbAUcQ2hlY2sgbXkgZWxlY3RvcmFsIGVucm9sbWVudGQCDg9kFgICAQ9kFgYCAw9kFgYCAQ8PFgIeC05hdmlnYXRlVXJsBTZodHRwOi8vd3d3LmFlYy5nb3YuYXUvRW5yb2xsaW5nX3RvX3ZvdGUvY2hlY2staGVscC5odG1kZAIDDw8WAh8CBTlodHRwOi8vd3d3LmFlYy5nb3YuYXUvQWJvdXRfQUVDL0NvbnRhY3RfdGhlX0FFQy9pbmRleC5odG1kZAIfDxBkZBYAZAIFDw8WAh4HVmlzaWJsZWhkFgQCBQ8PFgIfAgU5aHR0cDovL3d3dy5hZWMuZ292LmF1L0Fib3V0X0FFQy9Db250YWN0X3RoZV9BRUMvaW5kZXguaHRtZGQCBw8PFgIfAgVhaHR0cHM6Ly9zZWN1cmUuYWVjLmdvdi5hdS9PbmxpbmVGb3Jtcy9FbnJvbG1lbnQvVHJhY2tpbmc/ZklkPTgzMDU2MDQwLTA1MjUtNDc0NS1iYzkxLWEwNGQwMGExMWM1ZGRkAgcPDxYCHwNoZBYGAgMPZBYCAgEPDxYCHwIFOWh0dHA6Ly93d3cuYWVjLmdvdi5hdS9BYm91dF9BRUMvQ29udGFjdF90aGVfQUVDL2luZGV4Lmh0bWRkAgUPZBYCAgEPDxYCHwIFOWh0dHA6Ly93d3cuYWVjLmdvdi5hdS9BYm91dF9BRUMvQ29udGFjdF90aGVfQUVDL2luZGV4Lmh0bWRkAhEPDxYCHwIFLmh0dHA6Ly93d3cuYWVjLmdvdi5hdS9lbnJvbC9jaGFuZ2UtYWRkcmVzcy5odG1kZAITDw8WAh4EVGV4dAURMjAgU2VwdGVtYmVyIDIwMTJkZGQZieWTHmjSL+tpyMSN/mR1enJXQw==",
-        "__EVENTVALIDATION"=>"/wEWHwKky9yaDwL2jZepBgK8uovuDgKkseu7BALM2qLBBwLl642+AgKQjeh+AoDR0PoBAp2Ano4PAsiElZcCAs3i0o4MAs3iso4MAp7RpJEPAsziio4MAsW3urgLAqS+5bsDArXXmc8EAvTizo4MArKF8Z0HArHR6PoBAuDi0o4MAv7i8o4MAv3ijo4MAv3iso4MAo3R7PoBArbXoc8EApCN6H4Cpanj8gkC042JpgoCivDVxQcC5NS6swgnASx9Pl1SpyX/oGSL59++ZbkjTQ==",
-         "ctl00$ContentPlaceHolderBody$textSurname"=>"Fowler",
-         "ctl00$ContentPlaceHolderBody$textGivenName"=>"Perryn",
-         "ctl00$ContentPlaceHolderBody$textFlatNumber"=>"",
-         "ctl00$ContentPlaceHolderBody$textStreetNumber"=>"",
-         "ctl00$ContentPlaceHolderBody$textStreetName"=>"St Kilda",
-         "ctl00$ContentPlaceHolderBody$comboStreetType"=>"",
-         "ctl00$ContentPlaceHolderBody$textPostcode"=>"3184",
-         "ctl00$ContentPlaceHolderBody$DropdownSuburb"=>"ELWOOD (VIC)",
-         "LBD_VCID_verifyenrolment_ctl00_contentplaceholderbody_captchacode"=> session[:captcha_id],
-         "ctl00$ContentPlaceHolderBody$CaptchaCodeTextBox"=> params[:captcha],
-         "ctl00$ContentPlaceHolderBody$buttonVerify"=>" Verify Enrolment"
-      },
-      :headers => {
-        "Cookie" => session[:aec_cookies]
-      }
-    }   
-    puts "************"
-    puts params[:captcha] 
-    puts "************"
-    render :inline => CheckEnrolmentController.post('/VerifyEnrolment.aspx', options)
+    #EnrolmentCheck.new(params[:person])
+    check = EnrolmentCheck.new({:surname => "Fowler", :given_names => "Perryn", :street_name => "St Kilda", :postcode => "3184", :suburb => "ELWOOD (VIC)"})
+    render :inline => check.result(params[:captcha], session[:captcha_id] , session[:aec_cookies])
   end
     
   
---
+++ app/models/enrolment_check.rb
@@ -0,0 +1,39 @@
+class EnrolmentCheck
+  include HTTParty
+  base_uri 'https://oevf.aec.gov.au'
+  
+  def initialize persons_details
+    @persons_details = persons_details
+  end
+  
+  def result(captcha_text, captcha_id, aec_cookies)
+    options = {
+      :body => {
+        "__LASTFOCUS" => "",
+        "ctl00_ContentPlaceHolderBody_ToolkitScriptManager1_HiddenField" => "",
+        "__EVENTTARGET"=>"",
+        "__EVENTARGUMENT"=>"",
+        "__VIEWSTATE"=>"/wEPDwUJNDY5MTk1MTI1D2QWAmYPZBYEAgEPZBYCAhsPFgIeB2NvbnRlbnQFHENoZWNrIG15IGVsZWN0b3JhbCBlbnJvbG1lbnRkAgMPZBYGAgwPFgIeCWlubmVyaHRtbAUcQ2hlY2sgbXkgZWxlY3RvcmFsIGVucm9sbWVudGQCDg9kFgICAQ9kFgYCAw9kFgYCAQ8PFgIeC05hdmlnYXRlVXJsBTZodHRwOi8vd3d3LmFlYy5nb3YuYXUvRW5yb2xsaW5nX3RvX3ZvdGUvY2hlY2staGVscC5odG1kZAIDDw8WAh8CBTlodHRwOi8vd3d3LmFlYy5nb3YuYXUvQWJvdXRfQUVDL0NvbnRhY3RfdGhlX0FFQy9pbmRleC5odG1kZAIfDxBkZBYAZAIFDw8WAh4HVmlzaWJsZWhkFgQCBQ8PFgIfAgU5aHR0cDovL3d3dy5hZWMuZ292LmF1L0Fib3V0X0FFQy9Db250YWN0X3RoZV9BRUMvaW5kZXguaHRtZGQCBw8PFgIfAgVhaHR0cHM6Ly9zZWN1cmUuYWVjLmdvdi5hdS9PbmxpbmVGb3Jtcy9FbnJvbG1lbnQvVHJhY2tpbmc/ZklkPTgzMDU2MDQwLTA1MjUtNDc0NS1iYzkxLWEwNGQwMGExMWM1ZGRkAgcPDxYCHwNoZBYGAgMPZBYCAgEPDxYCHwIFOWh0dHA6Ly93d3cuYWVjLmdvdi5hdS9BYm91dF9BRUMvQ29udGFjdF90aGVfQUVDL2luZGV4Lmh0bWRkAgUPZBYCAgEPDxYCHwIFOWh0dHA6Ly93d3cuYWVjLmdvdi5hdS9BYm91dF9BRUMvQ29udGFjdF90aGVfQUVDL2luZGV4Lmh0bWRkAhEPDxYCHwIFLmh0dHA6Ly93d3cuYWVjLmdvdi5hdS9lbnJvbC9jaGFuZ2UtYWRkcmVzcy5odG1kZAITDw8WAh4EVGV4dAURMjAgU2VwdGVtYmVyIDIwMTJkZGQZieWTHmjSL+tpyMSN/mR1enJXQw==",
+        "__EVENTVALIDATION"=>"/wEWHwKky9yaDwL2jZepBgK8uovuDgKkseu7BALM2qLBBwLl642+AgKQjeh+AoDR0PoBAp2Ano4PAsiElZcCAs3i0o4MAs3iso4MAp7RpJEPAsziio4MAsW3urgLAqS+5bsDArXXmc8EAvTizo4MArKF8Z0HArHR6PoBAuDi0o4MAv7i8o4MAv3ijo4MAv3iso4MAo3R7PoBArbXoc8EApCN6H4Cpanj8gkC042JpgoCivDVxQcC5NS6swgnASx9Pl1SpyX/oGSL59++ZbkjTQ==",
+         "ctl00$ContentPlaceHolderBody$textSurname"=> @persons_details[:surname],
+         "ctl00$ContentPlaceHolderBody$textGivenName"=> @persons_details[:given_names],
+         "ctl00$ContentPlaceHolderBody$textFlatNumber"=>"",
+         "ctl00$ContentPlaceHolderBody$textStreetNumber"=>"",
+         "ctl00$ContentPlaceHolderBody$textStreetName"=> @persons_details[:street_name],
+         "ctl00$ContentPlaceHolderBody$comboStreetType"=>"",
+         "ctl00$ContentPlaceHolderBody$textPostcode"=> @persons_details[:postcode],
+         "ctl00$ContentPlaceHolderBody$DropdownSuburb"=> @persons_details[:suburb],
+         "LBD_VCID_verifyenrolment_ctl00_contentplaceholderbody_captchacode"=> captcha_id,
+         "ctl00$ContentPlaceHolderBody$CaptchaCodeTextBox"=> captcha_text,
+         "ctl00$ContentPlaceHolderBody$buttonVerify"=>" Verify Enrolment"
+      },
+      :headers => {
+        "Cookie" => aec_cookies
+      }
+    }
+    
+    EnrolmentCheck.post("/VerifyEnrolment.aspx", options)
+    
+  end
+  
+end
