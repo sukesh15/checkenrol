4545e4c837c4bf79bf56aa2adb8545109283e2de
Perryn Fowler <perryn.fowler@gmail.com>
2013-01-09 11:23:38 +1100

    captcha flow working

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.10'
+gem 'rails', '3.2.11'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-
+gem 'httparty'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ app/models/captcha_image.rb
@@ -0,0 +1,30 @@
+require "base64"
+
+class DataUrlParser < HTTParty::Parser
+  SupportedFormats.merge!('image/jpeg' => :data_url)
+
+  def data_url
+    "data:image/jpeg;base64," + Base64.strict_encode64(body)
+  end
+end
+
+class CaptchaImage
+  include HTTParty
+  parser DataUrlParser
+  
+  attr_reader :captcha_id
+  
+  def initialize 
+    @captcha_id = Digest::MD5.hexdigest("alex" + rand.to_s)
+    @response = CaptchaImage.get("https://oevf.aec.gov.au/BotDetectCaptcha.ashx?get=image&c=verifyenrolment_ctl00_contentplaceholderbody_captchacode&t=#{@captcha_id}")
+  end
+  
+  def data_uri
+    @response.parsed_response
+  end
+  
+  def cookies
+    @response.headers['set-cookie']
+  end
+  
+end
