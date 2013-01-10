4a47b80799c7c652608c00e111ed509961707cd4
Alex Ong <aong@thoughtworks.com>
2013-01-10 13:54:56 +1100

    removed favicon links; added default route to form

--- config/routes.rb
+++ config/routes.rb
@@ -47,7 +47,7 @@ Greenrol::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'check_enrolment#show'
 
   # See how all your routes lay out with "rake routes"
 
