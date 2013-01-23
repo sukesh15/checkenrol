18e7e33fb812ebfb93ef016c40d50f2d2e3948c0
Perryn Fowler <perryn.fowler@gmail.com>
2013-01-23 20:32:35 +1100

    add greens subdomain route

--- config/routes.rb
+++ config/routes.rb
@@ -47,6 +47,7 @@ Greenrol::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  root :to => 'check_enrolment#show' , :organisation => 'greens', :constraints => { :subdomain => 'greens' } 
   root :to => 'check_enrolment#show' , :organisation => 'default'
 
  
